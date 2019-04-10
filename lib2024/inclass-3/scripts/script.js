2f951bcaf4af1612aa634246861961491278628c
colw0013 <colw0013@algonquinlive.com>
2019-04-10 15:28:28 -0400

    attempted in class assignment
    I was not able to complete this. I was too confused.

---
+++ lib2024/inclass-3/scripts/script.js
@@ -0,0 +1,10 @@
+window.onload = init;
+
+addEventListner("click", function() { alert("This is my event listemr"); })
+
+function resizeText(multiplier) {
+	if (document.body.style.fontSize == "12px") {
+		document.body.style.fontSize = "12px"
+	}
+}
+// okay for real, I do not understand what I am doing. I kept trying things and the javascript was not showing up on my webpage. Your email was not working so I have no idea what the heck I am doing.
